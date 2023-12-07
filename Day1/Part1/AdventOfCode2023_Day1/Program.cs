c26ee74030621a1646e4229edb51f230ef808458
Fabio Costa <fabio.costa89@gmail.com>
2023-12-07 11:32:18 +0100

    Day 1

---
+++ Day1/Part1/AdventOfCode2023_Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿List<string> inputExamples = [
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet"
+    ];
+
+List<int> digits = [];
+//inputExamples.ForEach(digit => digits.Add(GetDigits(digit)));
+
+ReadInputFile().ForEach(digit => digits.Add(GetDigits(digit)));
+
+Console.WriteLine(GetTotal(digits));
+
+List<string> ReadInputFile() => File.ReadAllLines(@"Input\input.txt").ToList();
+
+int GetDigits(string input)
+{
+    char[] chars = input.Where(char.IsDigit).ToArray();
+    string firstLast = string.Concat(chars[0],chars[chars.Length - 1]);
+    return Convert.ToInt32(firstLast);
+}
+
+int GetTotal(IEnumerable<int> inputValues)
+{
+    return inputValues.Sum();
+}
---
+++ Day1/Part2/AdventOfCode2023_Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+List<string> inputExamples = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen"
+];
+
+List<int> digits = [];
+//inputExamples.ForEach(digit => digits.Add(GetDigits(digit)));
+
+ReadInputFile().ForEach(digit => digits.Add(GetDigitWithRegex(digit)));
+
+Console.WriteLine(GetTotal(digits));
+
+List<string> ReadInputFile() => File.ReadAllLines(@"Input\input.txt").ToList();
+
+int GetDigitWithRegex(string input)
+{
+    Match digit1 = Regex.Match(input, @"\d|one|two|three|four|five|six|seven|eight|nine");
+    Match digit2 = Regex.Match(input, @"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft);
+
+    return ParseMatch(digit1.Value) * 10 + ParseMatch(digit2.Value);
+}
+
+int ParseMatch(string input) => input switch
+{
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+    var d => int.Parse(d)
+};
+
+int GetTotal(IEnumerable<int> inputValues)
+{
+    return inputValues.Sum();
+}
